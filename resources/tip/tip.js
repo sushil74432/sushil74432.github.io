d4cfe6266d497e8b79fb4167f3fb1cb6d06fb2e4
sushil74432 <sushil.techguy@gmail.com>
2023-03-03 14:31:29 -0600

    calculator logic change

--- resources/tip/tip.js
+++ resources/tip/tip.js
@@ -1,6 +1,6 @@
 function calcTip(){
     var total = document.getElementById("subtotal").value;
-    var tip = total/10;
-    document.getElementById("tip").value = tip;
-    document.getElementById("total").textContent = parseInt(total)+parseInt(tip);
+    var tip = document.getElementById("tip").value;
+    var tipAmt = parseInt(total)+ (tip/total)*100
+    document.getElementById("total").textContent = "$"+tipAmt;
 }
