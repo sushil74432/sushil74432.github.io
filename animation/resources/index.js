311b2858798e97285f8c98f337096966f0759691
sushil74432 <sushil.techguy@gmail.com>
2023-03-08 22:23:05 -0600

    animation project updated

---
+++ animation/resources/index.js
@@ -0,0 +1,64 @@
+// var index = 0;
+var play;
+var curText;
+$(document).ready(function() {
+    // console.log(ANIMATIONS)
+    // ANIMATIONS.forEach((v, i) => {
+    //     console.log("here");
+    //     console.log(i);
+    // });
+
+    $("#type").bind("change", function(){
+        var val = $("#type").val();
+        // console.log(ANIMATIONS[val]);
+        $("#animationBox").val(ANIMATIONS[val]);
+    });
+    
+    $("#size").bind("change", function(){
+        var val = $("#size").val();
+        $("#animationBox").css("font-size", val);
+    });
+
+    $("#start").bind("click", runAnimation);
+    $("#stop").bind("click", stopAnimation);
+
+    $("#speed").bind("click", function(){
+        console.log("clicked turbo");
+        clearInterval(play);
+        runAnimation();
+    });
+});
+
+function runAnimation(){
+    $('#type').prop('disabled', true);
+    $('#start').prop('disabled', true);
+    $('#animationBox').prop('readonly', true);
+    $('#stop').prop('disabled', false);
+
+    // $('#size').prop('disabled', true);
+
+    var speed = $('#speed').is(":checked") ? 50 : 250;
+    console.log("Running Animation with frame rate : "+speed);
+    var index = 0;
+    var type = $("#type").val()
+    // var frames = ANIMATIONS[type].split("=====\n");
+    curText = $("#animationBox").val()
+    var frames = curText.split("=====\n")
+    play = setInterval(() => {
+        console.log("Running Animation");
+        if(index >= frames.length){
+            index = 0;
+        }
+        $("#animationBox").val(frames[index]);
+        index++;
+    }, speed);
+}
+
+function stopAnimation(){
+    $('#type').prop('disabled', false);
+    $('#size').prop('disabled', false);
+    $('#start').prop('disabled', false);
+    $('#stop').prop('disabled', true);
+    clearInterval(play);
+    $("#animationBox").val(curText);
+}
