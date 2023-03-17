263744907ecd1f3d101260d3a06e17d17cf53dc9
sushil74432 <sushil.techguy@gmail.com>
2023-03-17 14:23:00 -0500

    daily astrology pic of the day

---
+++ astro-pic/public/js/apod.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#view_button").click(getPicture);
+});
+function getPicture() {
+  $.ajax({
+    url: "https://api.nasa.gov/planetary/apod",
+    type: "GET",
+    data: { api_key: "DEMO_KEY", date: $("#date").val() },
+    dataType: "json",
+    success: showPicture,
+    error: noPicture,
+  });
+}
+function showPicture(data) {
+  $("#pic").attr("src", data.url);
+  $("#title").html(data.title);
+}
+function noPicture(error) {
+  alert(error.responseText);
+}
